6ed274926cba1f964401878ca5edb7464ec55bed
zhangbo2903879959 <2903879959@qq.com>
2025-12-22 17:34:19 +0800

    haha

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,9 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.entity.User;
 import com.example.demo.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,9 +11,16 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserMapper userMapper;
-
-    @RequestMapping(value = "/user",method = RequestMethod.GET)
+    @RequestMapping(value = "/userpage",method = RequestMethod.GET)
     public List<User> query(){
         return userMapper.selectList(null);
     }
+
+    @RequestMapping(value = "/userpage/adduser",method = RequestMethod.POST)
+    public String adduser(@RequestBody User user){// 自动把 dto 的 name/email 复制到 user
+        userMapper.insert(user);
+        return "用户创建成功，ID: " + user.getId(); // MP 会自动回填自增 ID
+    }
+
+
 }
